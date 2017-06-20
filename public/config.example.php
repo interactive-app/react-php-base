fecaa67070aa3d1b58459f96566074b7bbde11f4
Ankith Konda <ankith.konda@gmail.com>
2017-06-20 14:32:32 +1000

    Add API db and config scripts

---
+++ public/config.example.php
@@ -0,0 +1,12 @@
+<?php
+    $config = array(
+        'use_db'=>true,
+        'db'=>array(
+            'driver'=>'mysql',
+            'hostname'=>'localhost',
+            'username'=>'YOUR_DB_USERNAME',
+            'password'=>'YOUR_DB_PASSWORD',
+            'dbname'=>'YOUR_DB_NAME',
+        )
+    );
+?>
